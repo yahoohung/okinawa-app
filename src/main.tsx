770f753f59834d8f67b32c6a6dce456bf35e46dc
Marshall Chan <yahoohung@gmail.com>
2026-03-30 02:54:32 +0000

    updated  og image path

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { render } from 'preact'
 import './index.css'
 import { App } from './app.tsx'
+import heroImage from './assets/hero1.jpg'
+
+// Set Open Graph image dynamically so Vite can hash it
+const ogImageMeta = document.querySelector('meta[property="og:image"]')
+if (ogImageMeta) {
+  ogImageMeta.setAttribute('content', heroImage)
+}
 
 render(<App />, document.getElementById('app')!)
